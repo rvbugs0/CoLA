bc5173934c2c226e080abfbd67e6c6bf174312d0
Ravi Ailani <rvbugs0@gmail.com>
2023-01-04 04:38:48 +0530

    Updated comments on run.py

--- run.py
+++ run.py
@@ -19,28 +19,52 @@ os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
 # Set argument
 parser = argparse.ArgumentParser(description='CoLA: Self-Supervised Contrastive Learning for Anomaly Detection')
 parser.add_argument('--dataset', type=str, default='cora')  # 'BlogCatalog'  'Flickr'  'ACM'  'cora'  'citeseer'  'pubmed'
+
+# learning rate
 parser.add_argument('--lr', type=float)
+
 parser.add_argument('--weight_decay', type=float, default=0.0)
 parser.add_argument('--seed', type=int, default=1)
+
+# embedding dimensions - 64
 parser.add_argument('--embedding_dim', type=int, default=64)
+
+# no of epochs - complete training cycle - whole dataset
 parser.add_argument('--num_epoch', type=int)
+
+# ????
 parser.add_argument('--drop_prob', type=float, default=0.0)
+
+# batch size
 parser.add_argument('--batch_size', type=int, default=300)
+
+# subgraph size
 parser.add_argument('--subgraph_size', type=int, default=4)
+
+# readout module
 parser.add_argument('--readout', type=str, default='avg')  #max min avg  weighted_sum
+
+# R
 parser.add_argument('--auc_test_rounds', type=int, default=256)
+
+#  i think negative to positive sample ratio
 parser.add_argument('--negsamp_ratio', type=int, default=1)
 
 args = parser.parse_args()
 
+# learning rate = page 10 of paper - bottom left
 if args.lr is None:
     if args.dataset in ['cora','citeseer','pubmed','Flickr']:
+        # 0.001
         args.lr = 1e-3
     elif args.dataset == 'ACM':
+        # 0.0005
         args.lr = 5e-4
     elif args.dataset == 'BlogCatalog':
+        # 0.003
         args.lr = 3e-3
 
+# no of epochs = page 10 of paper - bottom left
 if args.num_epoch is None:
     if args.dataset in ['cora','citeseer','pubmed']:
         args.num_epoch = 100
@@ -51,6 +75,9 @@ batch_size = args.batch_size
 subgraph_size = args.subgraph_size
 
 print('Dataset: ',args.dataset)
+
+# ----------------------
+
 
 
 # Set random seed
@@ -69,16 +96,34 @@ torch.backends.cudnn.benchmark = False
 adj, features, labels, idx_train, idx_val,\
 idx_test, ano_label, str_ano_label, attr_ano_label = load_mat(args.dataset)
 
+
+
+
+# adj = adjacency matrix - network
+# features = Attribute vector
+# labels  = 
+# idx_train = training sample indices
+# idx_val,
+# idx_test = testing sample indexes
+# ano_label = anomally label
+# str_ano_label = structural anomally label 
+# attr_ano_label = attribute anomally label
+
+
 features, _ = preprocess_features(features)
-
 dgl_graph = adj_to_dgl_graph(adj)
-
 nb_nodes = features.shape[0]
 ft_size = features.shape[1]
+
+# can't understand this
 nb_classes = labels.shape[1]
 
+# Can't understand why did we normalize
 adj = normalize_adj(adj)
+
+# adding self loops i think
 adj = (adj + sp.eye(adj.shape[0])).todense()
+
 
 features = torch.FloatTensor(features[np.newaxis])
 adj = torch.FloatTensor(adj[np.newaxis])
@@ -87,8 +132,16 @@ idx_train = torch.LongTensor(idx_train)
 idx_val = torch.LongTensor(idx_val)
 idx_test = torch.LongTensor(idx_test)
 
+
+# ------------------------------
+
+
 # Initialize model and optimiser
+
+# RelU is the activation function
 model = Model(ft_size, args.embedding_dim, 'prelu', args.negsamp_ratio, args.readout)
+
+# Optimiser is Adam
 optimiser = torch.optim.Adam(model.parameters(), lr=args.lr, weight_decay=args.weight_decay)
 
 if torch.cuda.is_available():
@@ -101,28 +154,47 @@ if torch.cuda.is_available():
     idx_val = idx_val.cuda()
     idx_test = idx_test.cuda()
 
+
+
 if torch.cuda.is_available():
     b_xent = nn.BCEWithLogitsLoss(reduction='none', pos_weight=torch.tensor([args.negsamp_ratio]).cuda())
 else:
     b_xent = nn.BCEWithLogitsLoss(reduction='none', pos_weight=torch.tensor([args.negsamp_ratio]))
+
+
 xent = nn.CrossEntropyLoss()
 cnt_wait = 0
+
+# 1000000000.0
 best = 1e9
+
 best_t = 0
+
+
 batch_num = nb_nodes // batch_size + 1
+
+
 
 added_adj_zero_row = torch.zeros((nb_nodes, 1, subgraph_size))
 added_adj_zero_col = torch.zeros((nb_nodes, subgraph_size + 1, 1))
 added_adj_zero_col[:,-1,:] = 1.
+
+# possibly constructing zero vector for initial node in subgraph - anonymization
 added_feat_zero_row = torch.zeros((nb_nodes, 1, ft_size))
+
+
 if torch.cuda.is_available():
     added_adj_zero_row = added_adj_zero_row.cuda()
     added_adj_zero_col = added_adj_zero_col.cuda()
     added_feat_zero_row = added_feat_zero_row.cuda()
 
-# Train model
+# ----------------------------
+
+# Train model - tqdm progress bar
 with tqdm(total=args.num_epoch) as pbar:
     pbar.set_description('Training')
+
+    # Training for epochs - complete dataset = one epoch
     for epoch in range(args.num_epoch):
 
         loss_full_batch = torch.zeros((nb_nodes,1))
@@ -135,7 +207,9 @@ with tqdm(total=args.num_epoch) as pbar:
         random.shuffle(all_idx)
         total_loss = 0.
 
+        # total number of subgraphs generated = total no of nodes.
         subgraphs = generate_rwr_subgraph(dgl_graph, subgraph_size)
+
 
         for batch_idx in range(batch_num):
 
@@ -150,13 +224,18 @@ with tqdm(total=args.num_epoch) as pbar:
 
             cur_batch_size = len(idx)
 
+            # labels : 0 for negative and 1 for positive instance pairs
             lbl = torch.unsqueeze(torch.cat((torch.ones(cur_batch_size), torch.zeros(cur_batch_size * args.negsamp_ratio))), 1)
-
+            
             ba = []
             bf = []
+
             added_adj_zero_row = torch.zeros((cur_batch_size, 1, subgraph_size))
             added_adj_zero_col = torch.zeros((cur_batch_size, subgraph_size + 1, 1))
+            
+            # adding a new node to the subgraph??
             added_adj_zero_col[:, -1, :] = 1.
+            
             added_feat_zero_row = torch.zeros((cur_batch_size, 1, ft_size))
 
             if torch.cuda.is_available():
@@ -166,26 +245,54 @@ with tqdm(total=args.num_epoch) as pbar:
                 added_feat_zero_row = added_feat_zero_row.cuda()
 
             for i in idx:
+                # ************************************************ #
+                # very important line, how is shuffled indexes and subgraphs associated
+                
+                # what does this line do??
+
                 cur_adj = adj[:, subgraphs[i], :][:, :, subgraphs[i]]
+
                 cur_feat = features[:, subgraphs[i], :]
                 ba.append(cur_adj)
                 bf.append(cur_feat)
-
+            
+            # converted from 3D to 2D matrix
             ba = torch.cat(ba)
+            
+            # adding extra last zero row and zero col in all ba's and bf's 
+            # why ????????????????
             ba = torch.cat((ba, added_adj_zero_row), dim=1)
             ba = torch.cat((ba, added_adj_zero_col), dim=2)
-            bf = torch.cat(bf)
-            bf = torch.cat((bf[:, :-1, :], added_feat_zero_row, bf[:, -1:, :]),dim=1)
 
+            
+
+            # ------------------------------------
+            bf = torch.cat(bf)
+            
+            # attribute vector = zero for initial/target node in subgraph
+            # very weird
+
+            # converted from a 3D matrix to a 2-D matrix and 
+            # also added zero row in second last position
+            bf = torch.cat((bf[:, :-1, :], added_feat_zero_row, bf[:, -1:, :]),dim=1)
+            # ------------------------------------
+            
             logits = model(bf, ba)
             loss_all = b_xent(logits, lbl)
-
             loss = torch.mean(loss_all)
+
+            # logits and loss all - both have size 600 = size of labels = batch_size*2
+            # probably the negative sample is coming automatically from the model
 
             loss.backward()
             optimiser.step()
 
+            # loss for each batch 
             loss = loss.detach().cpu().numpy()
+
+
+
+            # only considering loss from positive samples
             loss_full_batch[idx] = loss_all[: cur_batch_size].detach()
 
             if not is_final_batch:
@@ -197,6 +304,7 @@ with tqdm(total=args.num_epoch) as pbar:
             best = mean_loss
             best_t = epoch
             cnt_wait = 0
+            # saving the improved model state in a file
             torch.save(model.state_dict(), 'best_model.pkl')
         else:
             cnt_wait += 1
@@ -204,13 +312,18 @@ with tqdm(total=args.num_epoch) as pbar:
         pbar.set_postfix(loss=mean_loss)
         pbar.update(1)
 
+# -------------------
 
 # Test model
 print('Loading {}th epoch'.format(best_t))
 model.load_state_dict(torch.load('best_model.pkl'))
 
 multi_round_ano_score = np.zeros((args.auc_test_rounds, nb_nodes))
+
+# positive instance scores
 multi_round_ano_score_p = np.zeros((args.auc_test_rounds, nb_nodes))
+
+# negative instance scores
 multi_round_ano_score_n = np.zeros((args.auc_test_rounds, nb_nodes))
 
 with tqdm(total=args.auc_test_rounds) as pbar_test:
@@ -263,16 +376,14 @@ with tqdm(total=args.auc_test_rounds) as pbar_test:
             with torch.no_grad():
                 logits = torch.squeeze(model(bf, ba))
                 logits = torch.sigmoid(logits)
+                
 
+            # difference of negative and positive pairs
             ano_score = - (logits[:cur_batch_size] - logits[cur_batch_size:]).cpu().numpy()
-            # ano_score_p = - logits[:cur_batch_size].cpu().numpy()
-            # ano_score_n = logits[cur_batch_size:].cpu().numpy()
-
             multi_round_ano_score[round, idx] = ano_score
-            # multi_round_ano_score_p[round, idx] = ano_score_p
-            # multi_round_ano_score_n[round, idx] = ano_score_n
 
         pbar_test.update(1)
+
 
 ano_score_final = np.mean(multi_round_ano_score, axis=0)
 # ano_score_final_p = np.mean(multi_round_ano_score_p, axis=0)
@@ -280,6 +391,4 @@ ano_score_final = np.mean(multi_round_ano_score, axis=0)
 auc = roc_auc_score(ano_label, ano_score_final)
 
 print('AUC:{:.4f}'.format(auc))
-
-
 
